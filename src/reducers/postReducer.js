651fabf415c41e3cdba1e8463b6f91a34977072a
Ngoc Nguyen <ngoc.ngyen.dev@gmail.com>
2019-01-10 17:38:11 +1000

    Finished create post & compose mutil enhancer

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -8,11 +8,16 @@ const initialState = {
 export default function(state = initialState, action) {
   switch(action.type) {
     case FETCH_POSTS:
-      console.log('reducer');
+    //console.log('reducer')
       return {
         ...state,
         items: action.payload
-      }
+      };
+    case NEW_POST:
+      return {
+        ...state,
+        item: action.payload
+      };
     default:
       return state;
   }
